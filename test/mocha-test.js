fac40fb223d4506338c8d05ee0dc0d1869d41a6b
Larry Battle <larry.battle.tech@gmail.com>
2014-02-21 18:13:13 -0600

    Update mocha-test.js

--- test/mocha-test.js
+++ test/mocha-test.js
@@ -1,17 +1,8 @@
-// Sample Mocha Test
-
-var chai = require("chai");
+// Test
+if(!chai && this.require){
+	var chai = this.require("chai");
+}
 var assert = chai.assert;
-var expect = chai.expect;
 
-describe("Array", function(){
-  describe("#indexOf()", function(){
-    it("should return -1 when the value is not present", function(){
-      var arr = [];
-      assert.equal(-1, arr.indexOf(5));
-      assert.equal(-1, arr.indexOf(0));
-      expect(arr).to.been.that.a("string");
-    });
-    it("should return  when the value is not present");
-  });
-});
+
+
