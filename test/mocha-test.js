f4f3930dfd4bbdaebe29998278afd4820138bdbc
Larry Battle <larry.battle.tech@gmail.com>
2014-02-23 15:21:17 -0600

    Update mocha-test.js

--- test/mocha-test.js
+++ test/mocha-test.js
@@ -1,7 +1,7 @@
 /**
-* Uses mocha cli to run the mocha test cases.
-* requires node.js 0.10.x
-*/
+ * Uses mocha cli to run the mocha test cases.
+ * requires node.js 0.10.x
+ */
 var fs = require("fs");
 var util = require("util");
 var path = require("path");
@@ -9,31 +9,31 @@ var chai = require("chai");
 var assert = chai.assert;
 
 var names = [
-  "ArrayUtil",
-  "BinaryFunction",
-  "BinaryGroupTable",
-  "CoverageTable",
-  "NumberUtil",
-  "Petrick",
-  "SimpleSet",
-  "StringUtil"
+	"ArrayUtil",
+	"BinaryFunction",
+	"BinaryGroupTable",
+	"CoverageTable",
+	"NumberUtil",
+	"Petrick",
+	"SimpleSet",
+	"StringUtil"
 ];
 var paths = {
-  "src": path.join(__dirname, "../src/"),
-  "tests": path.join(__dirname, "/js/")
+	"src" : path.join(__dirname, "../src/"),
+	"tests" : path.join(__dirname, "/js/")
 };
 var context = [assert];
 var contextNames = ["assert"];
 
-// read and compress all the source files into one string.
-var qm_src = names.map(function( name ){
-  var filepath = paths.src + name + ".js";
-  return fs.readFileSync( filepath, "utf8" );
-});
-// run each test by wrapping the source in a function that passes in all the neccessary function.
-var qm_tests_src = names.map(function( name ){
-  return fs.readFileSync( paths.tests + name + "-test.js", "utf8");
-});
-
-var qm_src_test_fn = new Function(contextNames, [].concat(qm_src, qm_tests_src).join("\n;") );
-qm_src_test_fn.apply( {}, context);
+// compress all the modules into a single string.
+var qm_src = names.map(function (name) {
+		var filepath = paths.src + name + ".js";
+		return fs.readFileSync(filepath, "utf8");
+	});
+// compress all the test into a single string.
+var qm_tests_src = names.map(function (name) {
+		return fs.readFileSync(paths.tests + name + "-test.js", "utf8");
+	});
+// eval the source of modules and tests.
+var qm_src_test_fn = new Function(contextNames, [].concat(qm_src, qm_tests_src).join("\n;"));
+qm_src_test_fn.apply({}, context);
