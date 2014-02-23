906603ce2fbf03608f068c10cb3ed08285c231b0
Larry Battle <larry.battle.tech@gmail.com>
2014-02-23 15:21:37 -0600

    Update BinaryGroupTable.js

--- src/BinaryGroupTable.js
+++ src/BinaryGroupTable.js
@@ -1,16 +1,31 @@
-// require("ArrayUtil");
-// require("NumberUtil");
-// require("StringUtil");
-var BinaryGroupTable = function(nums){
+/**
+* BinaryGroupTable finds the prime implicants from a list of decimals.
+* This is done by the following:
+* 1. Convert decimals to binary strings with a fixed length.
+* 2. Group the binary values based on the amount of 1s.
+* 3. For element in each one group, compare one group to the higher one group.
+* 4. If there is exactly difference between the two binary strings, then mark the difference
+*   and add the new value to a new grouped set.
+* 5. Repeat step 3 - 5 until there isn't a new set.
+* 6. Return the elements that weren't used to create a new element in any sets.
+*/
+var BinaryGroupTable = function(nums, binaryLength){
   if(!ArrayUtil.isArray(nums) ||  (0 < nums.length && isNaN(nums[0]))){
     throw new Error("Must pass an array of numbers");
   }
+  if(isNaN(binaryLength) || binaryLength < 1){
+	throw new Error("Must pass the binary length as a non-zero positive number.");
+  }
   this.nums = nums;
+  this.binaryLength = +binaryLength;
   return this;
+};
+BinaryGroupTable.sortBinaryStringsFunc = function(a,b){
+    return StringUtil.count(a.binString, "1") - StringUtil.count(b.binString, "1");
 };
 var bgtp = BinaryGroupTable.prototype;
 bgtp.expandElements = function(){
-  var els = this.numbersToSortedChartElements(this.nums),
+  var els = this.numbersToSortedChartElements(),
    oneGroup, el, el2, terms, termsAsString, newBinString,
    LOOP_LIMIT = 1e5,
    count = 0,
@@ -23,6 +38,7 @@ bgtp.expandElements = function(){
     }
     el = els[i];
     oneGroup = 1 + el.amountOf1s;
+	//@todo Extract this part out to make this function more testable.
     for(var i2 = i+1, l2 = els.length; i2 < l2; i2++){
       el2 = els[i2];
       if( oneGroup < el2.amountOf1s || (el2.amountOf1s < el.amountOf1s) ){
@@ -66,20 +82,19 @@ bgtp.createChartElement = function(binString, terms ){
     amountOf1s : StringUtil.count(binString, "1")
   };
 };
-bgtp.numbersToChartElements = function(nums){
-  if(!ArrayUtil.isArray(nums)){
+
+bgtp.numbersToChartElements = function(){
+  if(!ArrayUtil.isArray(this.nums)){
     throw new Error("Must be an array");
   }
-  var bins = NumberUtil.decsToBins(nums);
-  var els = [];
-  for(var i = 0, l = nums.length; i < l; i++){
-    els.push( this.createChartElement( bins[i], [nums[i]] ));
+  
+  var els = [], b;
+  for(var i = 0, l = this.nums.length; i < l; i++){
+	b = NumberUtil.decToBin(this.nums[i], this.binaryLength);
+    els.push( this.createChartElement( b, [this.nums[i]] ));
   }
   return els;
 };
-BinaryGroupTable.sortBinaryStringsFunc = function(a,b){
-    return StringUtil.count(a.binString, "1") - StringUtil.count(b.binString, "1");
-};
-bgtp.numbersToSortedChartElements = function(nums){
-  return this.numbersToChartElements(nums).sort(BinaryGroupTable.sortBinaryStringsFunc);
+bgtp.numbersToSortedChartElements = function(){
+  return this.numbersToChartElements().sort(BinaryGroupTable.sortBinaryStringsFunc);
 };
