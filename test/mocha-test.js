78d611571bebd9898b249c36d32cf82c994b16ed
Larry Battle <larry.battle.tech@gmail.com>
2014-02-26 01:50:22 -0600

    Updated test script to run all files in src and tests dir

--- test/mocha-test.js
+++ test/mocha-test.js
@@ -1,39 +1,38 @@
-/**
- * Uses mocha cli to run the mocha test cases.
- * requires node.js 0.10.x
- */
+// Reads all the files in the src and then executes the test files in the test diretory.
+// Run `mocha mocha-test.js`
+// @requires mochajs and node.js 10+
 var fs = require("fs");
 var util = require("util");
 var path = require("path");
 var chai = require("chai");
 var assert = chai.assert;
-
-var names = [
-	"ArrayUtil",
-	"BinaryFunction",
-	"BinaryGroupTable",
-	"CoverageTable",
-	"NumberUtil",
-	"Petrick",
-	"SimpleSet",
-	"StringUtil"
-];
+var context = [ assert ];
+var contextNames = [ "assert" ];
 var paths = {
-	"src" : path.join(__dirname, "../src/"),
-	"tests" : path.join(__dirname, "/js/")
+    src: path.join(__dirname, "../src/"),
+    tests: path.join(__dirname, "/js/")
 };
-var context = [assert];
-var contextNames = ["assert"];
 
-// compress all the modules into a single string.
-var qm_src = names.map(function (name) {
-		var filepath = paths.src + name + ".js";
-		return fs.readFileSync(filepath, "utf8");
-	});
-// compress all the test into a single string.
-var qm_tests_src = names.map(function (name) {
-		return fs.readFileSync(paths.tests + name + "-test.js", "utf8");
-	});
-// eval the source of modules and tests.
-var qm_src_test_fn = new Function(contextNames, [].concat(qm_src, qm_tests_src).join("\n;"));
+var mapFilesInDir = function(filepath, fn) {
+    var arr = [];
+    var files = fs.readdirSync(filepath);
+    for (var i in files) {
+        if (!files.hasOwnProperty(i)) {
+            continue;
+        }
+        arr.push(fn(filepath + "/" + files[i], i, files));
+    }
+    return arr;
+};
+
+var getSourceFromFilesInDir = function(dirPath) {
+    return mapFilesInDir(dirPath, function(filepath) {
+        return fs.readFileSync(filepath, "utf8");
+    });
+};
+
+var qm_src = getSourceFromFilesInDir(paths.src);
+var qm_tests = getSourceFromFilesInDir(paths.tests);
+var qm_src_test_fn = new Function(contextNames, [].concat(qm_src, qm_tests).join("\n;"));
+
 qm_src_test_fn.apply({}, context);
